9ccecddb6f06ec9b7121eca0c28508bd760c89f2
codewithzaman <naderzaman50@gmail.com>
2023-09-26 20:06:24 +0500

    Redux-Toolkit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { decrement, increment, incrementByAmount } from './features/counter/counterSlice';
 
 function App() {
+  const count = useSelector((state)=>state.counter.value);
+  const dispatch = useDispatch()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div><div className=' flex space-x-3 justify-center items-center h-screen'>
+   <div className="">
+
+     {/* Read  */}
+     <p className=' text-6xl font-bold text-center mb-5 text-black'>{count}</p>
+
+     {/* Increment Button  */}
+     <button
+       onClick={()=>dispatch(increment())} className=' bg-red-600 text-white py-2.5 rounded-md px-5 font-bold'
+     >
+       Increment
+     </button>
+
+     {/* Decrement Button  */}
+     <button onClick={()=>dispatch(decrement())}
+       className=' ml-20 bg-green-600 text-white py-2.5 rounded-md px-5 font-bold'
+     >
+       Decrement
+     </button>
+
+     {/* Increment By User */}
+     <button onClick={()=> dispatch(incrementByAmount(5))}
+       className=' ml-20 bg-orange-600 text-white py-2.5 rounded-md px-5 font-bold'
+     >
+       Inc. By User
+     </button>
+   </div>
+ </div></div>
   );
 }
 
